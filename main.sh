93141e41a932e993a39f2226c13d350eb2e6c191
Yohan <yohan_le_quere@ext.carrefour.com>
2022-12-12 18:08:18 +0100

    commit2022-12-12

---
+++ main.sh
@@ -0,0 +1,22 @@
+git init
+
+touch junk
+
+DATE="2022-12-12"
+end_date_secs=$(date -d "2024-03-26" +%s)
+
+while [ $(date -d "$DATE" +%s) -le $end_date_secs]
+do
+	echo 'a' >> junk
+
+        commit_date="$DATE 12:00:00"
+
+	git add .
+
+	msg='commit'${DATE}
+
+	git commit -m $msg --date="$DATE"
+   	
+   	DATE=$(date +%mY-%m-%d -d "$DATE + 1 day")
+
+done
